3ba6ddf277b1a234dbcf6d41ea92e5bb019d77b3
Wanderer22011 <22011@burnside.school.nz>
2024-06-13 11:02:12 +1200

    Added bullets and also updated movement control. Also added gun rotation where the mouse is pointing

---
+++ Characters/Area2D.gd
@@ -0,0 +1,13 @@
+extends Node2D # or whichever node you are using
+
+# Speed of rotation
+var rotation_speed = 20.0
+
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - global_position
+	var target_angle = direction.angle()
+	
+	# Interpolate the rotation smoothly
+	rotation = lerp_angle(rotation, target_angle, rotation_speed * delta)
+
--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -8,21 +8,28 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+
+enum State{Idle, Run, Jump, Shoot }
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func player_shooting(delta : float):
+
 
 	move_and_slide()
