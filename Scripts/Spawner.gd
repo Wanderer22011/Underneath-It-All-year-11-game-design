3d4e9d20df8eb17952652acd088e9e6a96627237
Wanderer22011 <22011@burnside.school.nz>
2024-07-23 23:12:12 +1200

    Tried to add spawner but didn't work. Still working in progress.

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -1,3 +1,10 @@
 extends Node2D
 
-@onready var enermy
+@onready var enemy = preload("res://Characters/enermy_from_spawner.tscn")
+
+
+
+func _on_timer_timeout():
+	var ene = enemy.instantiate()
+	ene.position = position
+	get_parent().get_node("EnemyFromSpawner").add_child(ene)
--- Scripts/enermy_from_spawner.gd
+++ Scripts/enermy_from_spawner.gd
@@ -1,19 +1,27 @@
 extends CharacterBody2D
 
-@export var speed = 500 
+@export var speed = 50
 var player_chase = false
 var player = null
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+		
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	if player_chase:
 		position.x += (player.position.x - position.x) / speed
 		
 		
 		
-func _on_detection_area_body_entered(body):
-	player = body
-	player_chase = true
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.name == "Main_Character":
+		player = body
+		player_chase = true
 
 
-func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	if body.name == "Main_Character":
+		player = null
+		player_chase = false
