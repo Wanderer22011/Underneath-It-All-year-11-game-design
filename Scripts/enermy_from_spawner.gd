15308fb444a177b50f9a5c448ae9298b8570e3da
Wanderer22011 <22011@burnside.school.nz>
2024-07-23 22:19:14 +1200

    Added new enermy to follow the player, although it is still in progress. Also cleaned up the folders.

---
+++ Scripts/enermy_from_spawner.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed = 500 
+var player_chase = false
+var player = null
+func _physics_process(delta):
+	if player_chase:
+		position.x += (player.position.x - position.x) / speed
+		
+		
+		
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body):
+	player = null
+	player_chase = false
